c5d6916f25cb3b8198f4eab6a0265ee306599b0e
SzymonOrynczak <szymon.orynczak@gmail.com>
2023-05-28 15:42:59 +0200

    opening project

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct Board
+{
+    int S;
+    int K;
+    int GW;
+    int GB;
+    int whitePawns;
+    int blackPawns;
+    char player;
+};
+
+enum BoardState {
+    BOARD_STATE_OK,
+    WRONG_WHITE_PAWNS_NUMBER,
+    WRONG_BLACK_PAWNS_NUMBER,
+    WRONG_BOARD_ROW_LENGTH
+};
+
+
+void readBoard()
+{
+
+    while()
+}
+
+
+
+int main()
+{
+    readBoard();
+}
